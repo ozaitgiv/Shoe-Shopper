f66609202a1badd5809533020d921c94764d4941
Saurab S <ajay.v.v.743@gmail.com>
2025-07-17 14:02:08 -0700

    S3 Integrated + req updated

--- backend/shoe_shopper/settings.py
+++ backend/shoe_shopper/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'shoe_shopper.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',         # Database Name
+        'NAME': 'postgres',            # Database Name
         'USER': 'postgres',            # Default PostgreSQL user
-        'PASSWORD': 'admin22@',                # variable Entry -- Change to Your Password 
+        'PASSWORD': 'admin22@',        # variable Entry -- Change to Your Password 
         'HOST': 'localhost',           
         'PORT': '5432',                # Default port
     }
@@ -113,6 +113,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Configuration for AWS S3 - Static and Media Files Storage
+
+AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
+AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+AWS_STORAGE_BUCKET_NAME = 'shoe-shopper-images'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Internationalization
