159541af0385d1150a126d8f7ef4206c56d1f98c
Armaan <82684913+armaan-costa@users.noreply.github.com>
2025-07-20 14:58:34 -0700

    Update serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import FootImage
+from .models import FootImage, Shoe
 
 class FootImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,4 +7,12 @@ class FootImageSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'image', 'uploaded_at', 'status',
             'length_inches', 'width_inches', 'error_message'
+        ]
+
+class ShoeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shoe
+        fields = [
+            'id', 'company', 'model', 'gender', 'us_size', 'width_category',
+            'function', 'price_usd', 'product_url', 'is_active'
         ]
